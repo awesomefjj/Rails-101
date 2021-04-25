5969832d4e4ef634218a5e856b6bae12c0a3df16
Jeffery <522023893@qq.com>
2021-04-25 15:41:28 +0800

    页面优化

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,9 +9,12 @@ class GroupsController < ApplicationController
 
     def create
         @group = Group.new(group_params)
-        @group.save
-
+        if @group.save
           redirect_to groups_path
+        else
+          render :new
+            
+          end
         
     end
 
@@ -25,10 +28,12 @@ class GroupsController < ApplicationController
 
     def update
         @group = Group.find(params[:id])
-        @group.update(group_params)
-
-        redirect_to  groups_path, notice: "Update Success"
-    end
+        if @group.update(group_params)
+         redirect_to  groups_path, notice: "Update Success"
+        else
+          render :edit  
+        end
+      end   
 
     def destroy
         @group = Group.find(params[:id])
